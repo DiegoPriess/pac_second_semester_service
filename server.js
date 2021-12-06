efe08d3b505d197f035b96065b319bd56b8b7ce8
Diego Priess <diegopriess.dev@gmail.com>
2021-12-06 12:59:44 -0300

    Configurando para integrar com front-end

--- server.js
+++ server.js
@@ -2,7 +2,14 @@ let express = require('express');
 const connection = require('./config/connection');
 let router = require('./routes/router');
 let server = express();
+const cors = require('cors');
+const corsOptions ={
+    origin:'http://localhost:3001', 
+    credentials:true,
+    optionSuccessStatus:200
+}
 
+server.use(cors(corsOptions));
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 server.use("/", router);
