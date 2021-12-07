23a57ac37c3291d646bb6513a902fa61b3b6feca
Diego Priess <diegopriess.dev@gmail.com>
2021-12-06 22:28:53 -0300

    Registro de contas buscando id do usuário

--- controller/accounts.js
+++ controller/accounts.js
@@ -5,8 +5,13 @@ let accounts = {};
 accounts.register = async function (req, res) {
   try {
     let data = req.body;
-    let query = `INSERT INTO accounts (id, description, month, year, price, type, status, id_users) VALUES(NULL, '${data.description}', '${data.month}', '${data.year}', '${data.price}', '${data.type}', '${data.status}', '${data.id_users}')`;
+
+    let queryGetUserId = `SELECT id FROM users WHERE email = '${data.currentUser.email}' AND password = '${data.currentUser.password}'`;
+    let user = await db_connect.query(queryGetUserId);
+
+    let query = `INSERT INTO accounts (id, description, month, year, price, type, status, id_users) VALUES(NULL, '${data.description}', '${data.month}', '${data.year}', '${data.price}', '${data.type}', '${data.status}', '${user[0].id}')`;
     let registerAccount = await db_connect.query(query);
+    
     res.send({
       status: 'success',
       result: registerAccount
